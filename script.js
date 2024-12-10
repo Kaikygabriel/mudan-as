2a27e75246d5a1dc1708cc412a99d6cb4bdf0c8d
Kaiky gabriel alves <kaikygabrielalves707@gmail.com>
2024-12-10 10:30:09 -0300

    attt

---
+++ script.js
@@ -0,0 +1,12 @@
+function clicous(){
+    window.scroll({
+        top: 380,
+        behavior:"smooth",
+    })
+}
+function sobre(){
+    window.scroll({
+        top: 880,
+        behavior:"smooth",
+    })
+}
